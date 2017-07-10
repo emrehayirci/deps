db838c09af981b846f1ca6f4f2008a4267266cec
Emre Hayırcı <emrehayirci@gmail.com>
2017-07-10 03:45:00 +0300

    Added Post, Comment, Upvote, Downvote models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.contrib.auth.models import User as DjangoUser
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    creation_date = models.DateTimeField()
+    author = models.ForeignKey(DjangoUser)
+
+class Comment:
+    body = models.TextField()
+    creation_date = models.DateTimeField()
+    author = models.ForeignKey(DjangoUser)
+
+class Upvote:
+    author = models.ForeignKey(DjangoUser)
+    item = models.ForeignKey(Post)
+
+class Downvote:
+    author = models.ForeignKey(DjangoUser)
+    item = models.ForeignKey(Post)
