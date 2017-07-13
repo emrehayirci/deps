4619a9eaf8b916a0e8e0a019a423af773595c4be
Emre Hayırcı <emrehayirci@gmail.com>
2017-07-13 20:41:22 +0300

    added list post view

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Post, Comment, Downvote, Upvote
 
-# Register your models here.
+
+admin.site.register(Post)
+#admin.site.register(Comment)
+#admin.site.register(Downvote)
+#admin.site.register(Upvote)
